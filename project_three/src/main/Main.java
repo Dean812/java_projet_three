85bb28c943adbc87a284ab919584f78e764ccdc2
Thibaut <tibeloute@msn.com>
2022-06-16 00:03:22 +0200

    Update Q4 to Q8

--- project_three/src/main/Main.java
+++ project_three/src/main/Main.java
@@ -9,11 +9,22 @@ public class Main {
 //		1- Créer une adresse dans la classe Main en utilisant le constructeur C1 .      
 //		2- Affecter des valeurs à l’objet adresse créée.
 //		3- Afficher l’objet adresse crée. 
-		
 		Adresse C1;   // # 1
-		C1 = new Adresse ("Jarvis Street"," Toronto",59184); // #2
+		C1 = new Adresse ("Jarvis Street"," Toronto",59000); // #2
 		System.out.println(C1);
 
+//		4- Modifier le code postal de cette adresse.
+//		5- Afficher l’objet adresse modifié.
+		C1.setCodePostal(59160);
+		System.out.println(C1);
+		
+//		6- Créer une adresse dans la classe Main en utilisant le constructeur C2
+//		7- Modifier la ville de cette adresse.
+//		8- Afficher l’objet adresse modifié.
+		Adresse C2 = new Adresse ("Rue Général leclercq", "Sainghin", 59184);
+		C2.setVille("Christchurch");
+		System.out.println(C2);
+		
 	}
 
 }
